31862bbbe0a556615d3699d45b957a86c16131cd
Reagan Wokorach <wokorachreagan5030@gmail.com>
2022-10-13 12:43:26 -0700

    Commit for n4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,7 @@
 #!/bin/bash
 #include <stdio.h>
-/**
- * main-entry point for the program.
- *
- * Return:0 if no errors, return non zero if errors
- * /
- int main(void)
- {
- printf("with proper programmer, but the outcome is a piece of art,\n");
- return(0);
- }
+int main(void)
+{
+	puts("\ "Programming is like building a multilingual puzzle");
+	return (0);
+}
