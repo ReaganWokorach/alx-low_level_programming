ef3179839f04d3fcb99280d7e6e13fe55353675d
Reagan Wokorach <wokorachreagan5030@gmail.com>
2022-10-16 11:28:38 -0700

    Commit for n6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Print size of various types
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("%lu\n", sizeof(a));
+	printf("%lu\n", sizeof(b));
+	printf("%lu\n", sizeof(c));
+	printf("%lu\n", sizeof(d));
+	printf("%lu\n", sizeof(e));
+
+	return (0);
+}
