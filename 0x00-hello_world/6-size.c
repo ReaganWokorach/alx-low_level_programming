8104147301328aec7c764dbe84c848097de2f00f
Reagan Wokorach <wokorachreagan5030@gmail.com>
2022-10-16 11:37:34 -0700

    Commit for n6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("%lu\n", sizeof(a));
-	printf("%lu\n", sizeof(b));
-	printf("%lu\n", sizeof(c));
-	printf("%lu\n", sizeof(d));
-	printf("%lu\n", sizeof(e));
+	printf("Size of a char: %lu byte(s)\n", sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", sizeof(e));
 
 	return (0);
 }
