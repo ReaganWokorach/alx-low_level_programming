3f7486b8b4afaa9e581e95fb4d27cab41601276f
Reagan Wokorach <wokorachreagan5030@gmail.com>
2022-10-17 10:23:21 -0700

    Commit for n0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Determine if a number is 0, positive or negative
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is postive\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
